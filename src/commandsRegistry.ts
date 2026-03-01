adbfea9e2e3cdbdaa69a9c083cfba52e2b930f90
mohammadsofan <nassersofan@gmail.com>
2026-03-02 01:44:25 +0200

    refactor: remove duplicate CommandsRegistry file

--- src/commandsRegistry.ts
+++
@@ -1,8 +0,0 @@
-import { CommandHandler } from "./commandHandler";
-
-export type CommandsRegistry = Record<string,CommandHandler>;
-export function runCommand(commandsRegistry:CommandsRegistry,cmdName:string,...args:string[]){
-    if(commandsRegistry[cmdName]){
-        commandsRegistry[cmdName](cmdName,...args);
-    }
-}
